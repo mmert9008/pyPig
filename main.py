0d35169ef6deae0f3090bfb5b66e00e0456e3c1b
mmert9008 <michael.mertens.jr@gmail.com>
2025-06-14 00:09:34 -0500

    implemented main function

--- main.py
+++ main.py
@@ -9,48 +9,55 @@ def roll():
     return roll
 
 
-while True:
-    num_players = input("Enter the number of players (2-4): ")
-    if num_players.isdigit():
-        num_players = int(num_players)
-        if 2 <= num_players <= 4:
-            break
-        else:
-            print("Must be between 2 and 4 players.")
-    else:
-        print("Please enter a valid number of players.")
-
-max_score = 50
-player_scores = [0 for _ in range(num_players)]
-
-while max(player_scores) < max_score:
-    for player_index in range(num_players):
-        print(f"\nPlayer {player_index + 1}'s turn:")
-        print(f"Your current score is {player_scores[player_index]}\n")
-        current_score = 0
-
-        while True:
-            wanna_roll = input(
-                f"Player {player_index + 1}, would you like to roll (y)? "
-            )
-            if wanna_roll.lower() != "y":
-                break
-
-            turn = roll()
-            if turn == 1:
-                print("You rolled a 1 and ended your turn")
-                current_score = 0
+def main():
+    while True:
+        num_players = input("Enter the number of players (2-4): ")
+        if num_players.isdigit():
+            num_players = int(num_players)
+            if 2 <= num_players <= 4:
                 break
             else:
-                current_score += turn
-                print(f"You rolled a {turn}")
+                print("Must be between 2 and 4 players.")
+        else:
+            print("Please enter a valid number of players.")
 
-            print(f"Your score is {current_score}")
+    max_score = 50
+    player_scores = [0 for _ in range(num_players)]
 
-        player_scores[player_index] += current_score
-        print(f"\nYour total score is {player_scores[player_index]}\n\n")
+    while max(player_scores) < max_score:
+        for player_index in range(num_players):
+            print(f"\nPlayer {player_index + 1}'s turn:")
+            print(f"Your current score is {player_scores[player_index]}\n")
+            current_score = 0
 
-max_score = max(player_scores)
-winner_index = player_scores.index(max_score)
+            while True:
+                wanna_roll = input(
+                    f"Player {player_index + 1}, would you like to roll (y)? "
+                )
+                if wanna_roll.lower() != "y":
+                    break
 
-print(f"Player {winner_index + 1} wins! Player {winner_index + 1} scored {max_score}!")
+                turn = roll()
+                if turn == 1:
+                    print("You rolled a 1 and ended your turn")
+                    current_score = 0
+                    break
+                else:
+                    current_score += turn
+                    print(f"You rolled a {turn}")
+
+                print(f"Your score is {current_score}")
+
+            player_scores[player_index] += current_score
+            print(f"\nYour total score is {player_scores[player_index]}\n\n")
+
+    max_score = max(player_scores)
+    winner_index = player_scores.index(max_score)
+
+    print(
+        f"Player {winner_index + 1} wins! Player {winner_index + 1} scored {max_score}!"
+    )
+
+
+if __name__ == "__main__":
+    main()
